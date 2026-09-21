c1798983426ae22fda6777dcc103eac850fbb661
erog873-afk <erog873@gmail.com>
2026-09-21 17:06:15 +0500

    Update Plinko.svelte

--- src/lib/components/Plinko/Plinko.svelte
+++ src/lib/components/Plinko/Plinko.svelte
@@ -19,29 +19,26 @@
   };
 </script>
 
-<!-- Труба стоит над полем: шарик выпадает из неё за верхним краем и падает на кегли -->
-<!-- -mt-10: труба ложится в свободное место над полем, а само поле поднимается выше -->
-<div class="relative -mt-10 pt-8">
+<div
+  class="relative overflow-hidden rounded-[40px] bg-gradient-to-b from-[#3d3e42] to-[#2b2c30] ring-1 ring-white/5"
+>
+  <div class="mx-auto flex h-full flex-col" style:max-width={`${WIDTH}px`}>
+    <div class="relative w-full" style:aspect-ratio={`${WIDTH} / ${HEIGHT}`}>
+      {#if $plinkoEngine === null}
+        <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <CircleNotch class="size-16 animate-spin text-white/30" weight="bold" />
+        </div>
+      {/if}
+
+      <canvas use:initPlinko width={WIDTH} height={HEIGHT} class="absolute inset-0 h-full w-full">
+      </canvas>
+    </div>
+    <BinsRow />
+  </div>
+
+  <!-- Жёлоб, из которого выпадает шарик -->
   <div
     aria-hidden="true"
-    class="absolute top-0 left-1/2 z-10 h-9 w-[11%] -translate-x-1/2 rounded-t-lg bg-gradient-to-r from-[#55565a] via-[#a0a1a5] to-[#55565a]"
+    class="absolute top-0 left-1/2 z-10 h-[10%] w-[11%] -translate-x-1/2 rounded-b-xl bg-gradient-to-r from-[#55565a] via-[#a0a1a5] to-[#55565a]"
   ></div>
-
-  <div
-    class="relative overflow-hidden rounded-[40px] bg-gradient-to-b from-[#3d3e42] to-[#2b2c30] ring-1 ring-white/5"
-  >
-    <div class="mx-auto flex h-full flex-col" style:max-width={`${WIDTH}px`}>
-      <div class="relative w-full" style:aspect-ratio={`${WIDTH} / ${HEIGHT}`}>
-        {#if $plinkoEngine === null}
-          <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <CircleNotch class="size-16 animate-spin text-white/30" weight="bold" />
-          </div>
-        {/if}
-
-        <canvas use:initPlinko width={WIDTH} height={HEIGHT} class="absolute inset-0 h-full w-full">
-        </canvas>
-      </div>
-      <BinsRow />
-    </div>
-  </div>
 </div>
